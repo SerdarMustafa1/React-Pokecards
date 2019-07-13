ba6395cb76d40944b018a98c9380aee26e2270aa
Serdar Mustafa <serdarmustafa@Serdar.local>
2019-07-13 19:03:57 +0300

    Updates

---
+++ precache-manifest.2460aa7db914415c14c23fc356ca009f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247af8b770c18bef97c53312aa268a13",
+    "url": "/React-Pokecards/index.html"
+  },
+  {
+    "revision": "a2ea78cf683fe797947d",
+    "url": "/React-Pokecards/static/css/main.0ee7613d.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/React-Pokecards/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "a2ea78cf683fe797947d",
+    "url": "/React-Pokecards/static/js/main.18af6aee.chunk.js"
+  },
+  {
+    "revision": "a35196dd0caa87448428",
+    "url": "/React-Pokecards/static/js/runtime~main.cbd4ccaf.js"
+  }
+]);
